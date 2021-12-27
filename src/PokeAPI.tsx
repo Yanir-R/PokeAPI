ee1f4dd8163f4eed2e0b57bc479f52d24aa0a9fe
Sheba467 <lowyouone@gmail.com>
2021-12-27 13:50:57 +0200

    adding pagination Next & Previous + css

--- src/PokeAPI.tsx
+++ src/PokeAPI.tsx
@@ -5,34 +5,26 @@ import { PokeList } from "./components/PokeList";
 
 export const PokeAPI: React.FC = () => {
     let [pokemonData, setPokemonData] = useState([])
-    let [displayedData, setDisplayedData] = useState([])
-    const [offset, setOffset] = useState(0);
+    let [pageNumber, setPageNumber] = useState<number>(1)
     let pokemonPerPage = 10;
-    const allPages = Math.ceil(pokemonData.length / pokemonPerPage)
-
-    const onPageChange = (page: number = 1) => {
-        let startItem = (page - 1) * pokemonPerPage;
-        let endItem = page * pokemonPerPage;
-        setDisplayedData(pokemonData.slice(startItem, endItem))
-    }
-
 
     useEffect(() => {
-        fetchPokemonData()
-        onPageChange()
-    }, [pokemonData.length])
+        fetchPokemonData(pageNumber)
+    }, [pageNumber])
+
 
     const fetchPokemonData = async (pageNumber: number) => {
 
         const pokemonResponse: any = await api.getAllPokemon(pokemonPerPage, (pageNumber - 1) * pokemonPerPage)
         setPokemonData(pokemonResponse)
     }
+
     return (
         <div>
 
             <h2>pokeAPI Page</h2>
-            <PokeList displayedData={displayedData} pokemonData={pokemonData} />
-            <Pagination allPagesNumber={allPages} itemsPerPage={10} itemsNumber={pokemonData.length} pageChange={onPageChange} />
+            <PokeList pokemonData={pokemonData} />
+            <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} />
 
         </div>
     )
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,59 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../App.css'
 
-interface IProps {
-    allPagesNumber: number
-    itemsPerPage: number
-    itemsNumber: number
-    pageChange: (page: number) => void
-}
-
-const Pagination: React.FC<IProps> = (props) => {
-    const [currentPage, setCurrentPage] = useState<number>(1);
-
-    useEffect(() => {
-        props.pageChange(currentPage)
-    }, [currentPage])
-
-    const onFirstPage = (): void => {
-        setCurrentPage(1)
-    }
-
-    const onLastPage = (): void => {
-        setCurrentPage(props.allPagesNumber)
-    }
+const Pagination: React.FC<any> = ({ setPageNumber, pageNumber }: any) => {
 
     const onNextPage = (): void => {
-        setCurrentPage(currentPage + 1)
+        setPageNumber(pageNumber + 1)
     }
 
     const onPreviousPage = (): void => {
-        setCurrentPage(currentPage - 1)
+        if (pageNumber === 1) return;
+        setPageNumber(pageNumber - 1)
     }
 
     return (
         <div>
-            <div className={`pagination__button pagination__page-first ${currentPage === 1 ? 'pagination__button--disabled' : ''}`}
-                onClick={() => onFirstPage()}>
-                First
-            </div>
-            <div className={`pagination__button pagination__page-previous ${currentPage === 1 && 'pagination__button--disabled'}`}
+
+            <div className={`pagination__button pagination__page-previous ${pageNumber === 1 && 'pagination__button--disabled'}`}
                 onClick={() => onPreviousPage()}>
                 Previous
             </div>
 
             <div>
-                <span>{props.allPagesNumber}</span>
+                {/* <span>{pageNumber}</span> */}
             </div>
-            <div className={`pagination__button pagination__page-next ${currentPage === props.allPagesNumber && 'pagination__button--disabled'}`}
+            <div className={`pagination__button pagination__page-next `}
                 onClick={() => onNextPage()}>
                 Next
             </div>
 
-            <div className={`pagination__button pagination__page-last ${currentPage === props.allPagesNumber && ' pagination__button--disabled'}`}
-                onClick={() => onLastPage()}>
-                Last
-            </div>
         </div>
     )
 }
